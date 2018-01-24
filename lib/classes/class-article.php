99db2146f93179ba4181052781090350211a7ddb
Lesa McPeak <lesa.mcpeak@wsu.edu>
2018-01-23 16:57:03 -0800

    fixed spacing

--- lib/classes/class-article.php
+++ lib/classes/class-article.php
@@ -12,39 +12,39 @@ if ( ! defined( 'WPINC' ) ) {
  * @since 0.0.1
  */
 class Article {
-	
+
 	// @var string Article title
 	protected $title = '';
-	
+
 	// @var string Article excerpt
 	protected $except = '';
-	
-	// @var string Article content 
+
+	// @var string Article content
 	protected $content = '';
-	
-	
+
+
 	/**
 	 * @desc Get article title
 	 * @since 0.0.1
 	 * @return string Article title
 	 */
 	public function get_title() {
-		
+
 		return $this->title;
-		
+
 	} // End get_title
-	
+
 	/**
 	* @desc Get article excerpt
 	* @since 0.0.1
 	* @return string Article excerpt
 	*/
 	public function get_excerpt() {
-		
+
 		return $this->excerpt;
-		
+
 	} //end get_excerpt
-	
+
 	//[TO DO] Add content
-	
+
 }
