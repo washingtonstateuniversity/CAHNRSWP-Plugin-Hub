90bb3ff929bff86bfc751b9f8f7cc0501f928ed0
Lesa McPeak <lesa.mcpeak@wsu.edu>
2018-01-23 16:54:10 -0800

    fixed spacing

--- lib/classes/class-article-factory.php
+++ lib/classes/class-article-factory.php
@@ -12,33 +12,33 @@ if ( ! defined( 'WPINC' ) ) {
  * @since 0.0.1
  */
 class Article_Factory {
-	
+
 	public function get_article( $type ) {
-		
-		switch( $type ) {
-			
+
+		switch ( $type ) {
+
 			case 'new-article':
 				$article = $this->get_news_article();
 				break;
 			default:
 				$article = false;
 				break;
-				
+
 		}
-		
+
 		return $article;
-		
+
 	}
-	
-	
+
+
 	protected function get_news_article() {
-		
+
 		include_once get_plugin_dir_path( 'lib/classes/class-news-article.php' );
-		
+
 		$article = new News_Article();
-		
+
 		return $article;
-		
+
 	}
-	
+
 } //end Article_Factory
