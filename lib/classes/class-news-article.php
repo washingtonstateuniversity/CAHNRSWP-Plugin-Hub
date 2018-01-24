31e6002b951f3e77f0935ab1c4a62e21a3b9305c
Lesa McPeak <lesa.mcpeak@wsu.edu>
2018-01-23 16:26:35 -0800

    Added News Article class

---
+++ lib/classes/class-news-article.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WSUWP\CAHNRS_Plugin_Hub;
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+} // End if
+
+/**
+ * @desc  Abstract Article class
+ * @since 0.0.1
+ */
+class News_Article extends Article {
+	
+	
+}
